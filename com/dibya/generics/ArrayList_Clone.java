20eadc3c333d3b6310cb27a2d359572e98788d5e
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-07-15 16:32:40 +0530

    Generics Part 1

--- com/dibya/generics/ArrayList_Clone.java
+++ com/dibya/generics/ArrayList_Clone.java
@@ -1,33 +1,31 @@
 package com.dibya.generics;
 
+import java.util.Arrays;
+
 //import java.util.ArrayList;
 
 public class ArrayList_Clone {
 
 	private int[] data;
-	private static int DEFAULT_SIZE = 5;
 	private int size = 0;
+	private static int DEFAULT_SIZE = 2;
 
 	public ArrayList_Clone() {
-
 		this.data = new int[DEFAULT_SIZE];
 	}
 
 	public void add(int num) {
-
 		if (isFull()) {
 			resize();
 		}
-
 		data[size++] = num;
 	}
 
 	private void resize() {
 
 		int[] temp = new int[data.length * 2];
-//		copy current items in new array
 
-		for (int i = 0; i < data.length; i++) {
+		for (int i = 0; i <= data.length - 1; i++) {
 			temp[i] = data[i];
 		}
 
@@ -39,10 +37,45 @@ public class ArrayList_Clone {
 		return size == data.length;
 	}
 
-	public static void main(String[] args) {
+	private int get(int index) {
 
-		ArrayList_Clone newarr = new ArrayList_Clone();
-		newarr.add(3);
+		return data[index];
+	}
+
+	private int set(int index, int value) {
+
+		data[index] = value;
+		return data[index];
+	}
+
+	private int remove() {
+
+		int removed = data[--size];
+		return removed;
+	}
+
+	public int length() {
+		return size;
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(data);
+	}
+
+	public static void main(String args[]) {
+
+//		ArrayList<String> list = new ArrayList<String>();
+//		list.add("Dibya");
+//		list.add("Anupol");
+//
+//		System.out.print(list.toString());
+
+		ArrayList_Clone arr = new ArrayList_Clone();
+		arr.add(3);
+		arr.add(4);
+		arr.add(5);
+		System.out.println(arr);
 
 	}
 
